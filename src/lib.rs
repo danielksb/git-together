b2da4c6284678c5835d4066d50021f68d23116a7
Daniel Kaes <danielkaes@live.com>
2016-11-24 22:27:03 +0100

    move code to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,52 @@
+use std::collections::HashMap;
+use std::process::*;
+use std::io::BufRead;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+    }
+}
+
+pub type MatchCount = HashMap<String, i32>;
+
+pub fn find_files_committed_together(target_filename: &str) -> MatchCount {
+    let log_output = Command::new("git")
+        .arg("log").arg("--name-only").arg("--pretty=format:")
+        .output().unwrap_or_else(|e| { panic!("failed to execute process '{}'", e) });
+
+    let mut matchcount: HashMap<String, i32> = HashMap::new();
+
+    let mut contains_target = false;
+    let mut commit: Vec<String> = Vec::new();
+
+    for line in log_output.stdout.lines() {
+        let filename = line.unwrap();
+        if filename.len() == 0 {
+            if contains_target {
+                for file in commit.iter() {
+                    let count = matchcount.entry(file.clone()).or_insert(0);
+                    *count += 1;
+                }
+                contains_target = false;
+            }
+            commit.clear();
+        } else {
+            if target_filename == filename {
+                contains_target = true;
+            } else {
+                commit.push(filename.clone());
+            }
+        }
+    }
+    return matchcount;
+} 
+
+pub fn print_matchcount(matchcount: &MatchCount) {
+    let mut entries: Vec<(&String, &i32)> = matchcount.iter().collect();
+    entries.sort_by(|e1, e2| e1.1.cmp(e2.1).reverse());
+    for entry in entries.iter().take(10) {
+        println!("{} {:>5}", entry.1, entry.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::process::*;
 use std::env;
-use std::io::BufRead;
-use std::collections::HashMap;
+
+mod lib;
 
 fn get_filename(args: &Vec<String>) -> Result<String, String> {
   let argument = args.iter().skip(1).next();
@@ -14,37 +13,6 @@ fn get_filename(args: &Vec<String>) -> Result<String, String> {
 fn main() {
     let args = env::args().collect();
     let target_filename = get_filename(&args).unwrap_or_else(|e| { panic!("cannot read arguments '{}'", e) });
-    let log_output = Command::new("git")
-        .arg("log").arg("--name-only").arg("--pretty=format:")
-        .output().unwrap_or_else(|e| { panic!("failed to execute process '{}'", e) });
-
-    let mut matchcount: HashMap<String, i32> = HashMap::new();
-
-    let mut contains_target = false;
-    let mut commit: Vec<String> = Vec::new();
-
-    for line in log_output.stdout.lines() {
-        let filename = line.unwrap();
-        if filename.len() == 0 {
-            if contains_target {
-                for file in commit.iter() {
-                    let count = matchcount.entry(file.clone()).or_insert(0);
-                    *count += 1;
-                }
-                contains_target = false;
-            }
-            commit.clear();
-        } else {
-            if target_filename == filename {
-                contains_target = true;
-            } else {
-                commit.push(filename.clone());
-            }
-        }
-    }
-    let mut entries: Vec<(&String, &i32)> = matchcount.iter().collect();
-    entries.sort_by(|e1, e2| e1.1.cmp(e2.1).reverse());
-    for entry in entries.iter().take(10) {
-        println!("{} {:>5}", entry.1, entry.0);
-    }
+    let matchcount = lib::find_files_committed_together(target_filename.as_str());
+    lib::print_matchcount(&matchcount);
 }
